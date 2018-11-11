43ad77b8f59e5043e48da830562773f5c09275b7
xhosaka <rayalwood@gmail.com>
2018-11-11 23:09:30 +0200

    comeback

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,17 @@
 import React, { Component } from 'react'
 import { ThemeProvider } from 'styled-components'
 
-import Header from './Header'
 import Player from './Player'
-import styled from 'styled-components'
+import Scrollable from './Scrollable'
 
 import { lightTheme } from '../theme/globalStyle'
 
-import Scrollable from './Scrollable'
-import 'react-perfect-scrollbar/dist/css/styles.css';
-
-import PerfectScrollbar from 'react-perfect-scrollbar'
-import SimpleExample from './Lol'
 class App extends Component {
 
   render() {
-    let scrollbarStyles = {borderRadius: 5};
     return (
       <ThemeProvider theme={lightTheme}>
         <div>
-          <div style={{
-            display: 'flex'
-          }}>
-          <div style={
-          {
-          margin: '50px',
-          width: '350px',
-          height: '250px',
-          background: '#f5f5f5',
-          border: '1px solid #333'
-          }
-          }>
-          <SimpleExample />
-          </div>
           <div style={
           {
           margin: '50px',
@@ -47,13 +26,9 @@ class App extends Component {
               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa qui beatae laboriosam expedita doloribus nemo, repellat, totam illo ipsum magni recusandae aperiam corporis mollitia, voluptas consectetur omnis? Fugiat, excepturi nobis?
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam mollitia modi ab repellendus magnam fugiat expedita nesciunt laborum, quaerat quam ipsa perspiciatis, inventore rerum eius nihil harum maiores error possimus.
             </Scrollable>
-          </div>
-          </div>
-
-        
-        <Player playlist={this.props.data} />
+          </div> 
+        {/* <Player playlist={this.props.data} /> */}
         </div>
-
       </ThemeProvider> 
     )
   }
--- src/components/Scrollable/ScrollBar.js
+++ src/components/Scrollable/ScrollBar.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { findDOMNode } from 'react-dom'
 import styled from 'styled-components'
 
 const Wrapper = styled.div`
@@ -26,45 +25,50 @@ const Thumb = styled.div`
   position: absolute;
   right: 0;
   top: ${({sliderPosition}) => sliderPosition}%;
-  height: ${({sliderHeight}) => sliderHeight}px;
+  height: ${({thumbHeight}) => thumbHeight}px;
   width: 100%;
   background-color: #ff7777;
   box-shadow: 1px 1px 1px rgba(0, 0, 0, .1);
   border-radius: 5px;
 `
 
-
 export default class ScrollBar extends Component {
   state = {
-    sliderHeight: null
+    thumbHeight: null
   }
 
-  getSliderRef = node => {
-    this.slider = node
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.viewportHeight !== this.props.viewportHeight) {
+      this.setState({
+        thumbHeight: this.getThumbHeight()
+      })
+    }
   }
 
-  getSliderHeight = () => {
+  getThumbHeight = () => {
     const { viewportHeight, contentHeight } = this.props
+
     return Math.round(viewportHeight / contentHeight * viewportHeight)
   }
   
-  getSliderPosition = () => {
-    const { viewportHeight, contentHeight, contentPosition } = this.props
+  getThumbPosition = () => {
+    const { viewportHeight, contentPosition, contentHeight } = this.props
 
-    return ((Math.abs(contentPosition))) / (viewportHeight) * 100
+    return (Math.abs(contentPosition) / (contentHeight - viewportHeight) * 100).toFixed(2)
   }
 
   render() {
-    const { viewportHeight, contentHeight, contentPosition } = this.props
+    const { viewportHeight } = this.props
+    const { thumbHeight } = this.state
 
     return (
       <Wrapper>
         <Track
-          trackHeight={viewportHeight - this.getSliderHeight()}
+          trackHeight={viewportHeight - thumbHeight}
         >
           <Thumb
-            sliderHeight={this.getSliderHeight()}
-            sliderPosition={this.getSliderPosition()}
+            thumbHeight={thumbHeight}
+            sliderPosition={this.getThumbPosition()}
           />
         </Track>
       </Wrapper>
@@ -73,7 +77,7 @@ export default class ScrollBar extends Component {
 }
 
 ScrollBar.propTypes = {
-  viewportHeight: PropTypes.number, 
+  viewportHeight: PropTypes.number,
   contentHeight: PropTypes.number,
   contentPosition: PropTypes.number
 }
